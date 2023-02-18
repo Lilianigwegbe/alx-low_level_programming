028a943980a7fe0f6ee4f9415583e09f62613be0
lilianigwegbe <lilianc.igwegbe@gmail.com>
2023-02-17 16:37:05 -0800

    7 Print tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-
 /**
  * main - Entry point
  *
+ * This is a simple C code,
+ * it prints all alphabets in lowercase,
+ * but in reverse order,
+ * using the putchar function.
+ *
  * Return: 0
 */
-
 int main(void)
 {
 	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
@@ -14,11 +17,7 @@ int main(void)
 	int i;
 
 	for (i = len; i >= 0; i--)
+	for (i = (len - 1); i >= 0; i--)
 	{
 		putchar(alphabets[i]);
 	}
-	putchar('\n');
-
-	return (0);
-}
-
