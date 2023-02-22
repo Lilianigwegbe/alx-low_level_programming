d66d418bec1a9697a4abc3cda4eaf8619d8aa53a
lilianigwegbe <lilianc.igwegbe@gmail.com>
2023-02-22 10:39:59 -0800

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,23 @@
+# include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * A program that prints _putchar, followed by a new line.
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
