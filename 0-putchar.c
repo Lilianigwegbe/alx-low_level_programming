05a87fd643d8342de45ebd11de613425d6a4bf04
lilianigwegbe <lilianc.igwegbe@gmail.com>
2023-02-22 11:00:12 -0800

    0-putchar.c

--- 0-putchar.c
+++ 0-putchar.c
@@ -1,11 +1,11 @@
 # include "main.h"
 
 /**
- * main - Entry point
- *
- * A program that prints _putchar, followed by a new line.
- *
- * Return: 0
+* main - Entry point
+*
+* A program that prints _putchar, followed by a new line.
+*
+* Return: 0
 */
 
 int main(void)
