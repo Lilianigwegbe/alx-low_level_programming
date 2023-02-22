8deb0f7a60cf9a60920664b23ab92d2b66e0e23d
lilianigwegbe <lilianc.igwegbe@gmail.com>
2023-02-21 19:55:48 -0800

    Putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#include <unistd.h>
-
 int _putchar(char c);
 
 void print_alphabet(void);
@@ -17,10 +15,11 @@ int _abs(int);
 int print_last_digit(int);
 
 void jack_bauer(void);
-
 void times_table(void);
 
 int add(int, int);
 
 void print_to_98(int n);
+
+void print_times_table(int n);
 
