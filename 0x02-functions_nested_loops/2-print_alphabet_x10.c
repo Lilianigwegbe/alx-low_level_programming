962217645773e5c098e10411f62cbc6ccc4de337
lilianigwegbe <lilianc.igwegbe@gmail.com>
2023-02-22 17:27:35 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,27 @@
 # include "main.h"
 
 /**
- * print_alphabet - Entry point
+ * print_alphabet_x10 - Entry point
  *
  * This is a simple C code,
  * it prints the alphabets in lowercase
+ * 10 times
  * using the custom _putchar function
  *
  * Return: 0
 */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char str[] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	int i, j;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(str[i]);
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(str[j]);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
