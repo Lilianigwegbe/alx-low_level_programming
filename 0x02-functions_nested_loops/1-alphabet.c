54e106ae13cdfe009af22cec1d21df292111ba07
lilianigwegbe <lilianc.igwegbe@gmail.com>
2023-02-22 20:49:42 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 # include "main.h"
 
 /**
-<<<<<<< HEAD
  * print_alphabet - Entry point
  *
  * This is a simple C code,
@@ -10,27 +9,15 @@
  *
  * Return: 0
 */
-=======
- * main - check the code
- *
- * Return: Always 0
- */
->>>>>>> 9fac56347d114c025ec52ca13d0fb451258ce2e9
 
 void print_alphabet(void)
 {
-<<<<<<< HEAD
-	char str[] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+char str[] = "abcdefghijklmnopqrstuvwxyz";
+int i;
 
-	for (i = 0; i < 26; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
-=======
-print_alphabet();
-return (0);
->>>>>>> 9fac56347d114c025ec52ca13d0fb451258ce2e9
+for (i = 0; i < 26; i++)
+{
+_putchar(str[i]);
 }
-
+_putchar('\n');
+}
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,41 +1,12 @@
-
-vi 4-isalpha.c
-
-
 #include "main.h"
 
 /**
-
- * _isalpha-check if character is lowercase or uppercase
-
- * @c:character to check if it islowercase.
-
- * Return:1-if lowercase/uppercase,0-if not.
-
+ * _isalpha - check for lower case letter
+ * @c : character to check
+ * Return:0 or 1
  */
 
-
-
-
 int _isalpha(int c)
-
 {
-
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-
-        {
-
-                return (1);
-
-        }
-
-        else
-
-        {
-
-                return (0);
-
-        }
-
+return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 }
-
