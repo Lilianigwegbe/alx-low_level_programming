0940f094cef9f77af9bd21ffb7f7c727b5ee1eac
lilianigwegbe <lilianc.igwegbe@gmail.com>
2023-02-22 20:14:53 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+# include "main.h"
+
+/**
+ * add - Entry point
+ * @a: first number to add
+ * @b: second number to add
+ *
+ * Return: 0
+*/
+
+int add(int a, int b)
+{
+int result;
+result = a + b;
+return (result);
+}
