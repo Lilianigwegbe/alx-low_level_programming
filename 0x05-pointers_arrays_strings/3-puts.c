164718cba3cd3ae2ca13b8e068192ed3cce5ad50
lilianigwegbe <lilianc.igwegbe@gmail.com>
2023-02-27 15:19:36 -0800

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ * @str: String to be printed.
+ *
+ * This program prints the characters
+ * of a string from a character pointer.
+ *
+ * return: Nothing
+ */
+void _puts(char *str)
+{
+int c = 0;
+while (*(str + c) != '\0')
+{
+_putchar(*(str + c));
+c++;
+}
+_putchar('\n');
+}
