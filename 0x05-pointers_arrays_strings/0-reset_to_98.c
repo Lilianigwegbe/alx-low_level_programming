7b2a4a2ff2341abe167e6d96866b5c159ae28be8
lilianigwegbe <lilianc.igwegbe@gmail.com>
2023-02-27 14:59:39 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * @n: pointer to be used
+ *
+ * The is a C programm.
+ * It changes the value at the address of
+ * a pointer.
+ *
+ * return: Nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
