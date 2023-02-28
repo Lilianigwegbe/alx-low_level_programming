a65e61684454d094c63d52e1d36be351c9243d6f
lilianigwegbe <lilianc.igwegbe@gmail.com>
2023-02-27 18:05:58 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,43 @@
-hw:
+#include "main.h"
+
+/**
+ * puts_half - Entry point
+ * @str: String to be printed.
+ *
+ * A function that prints half of a string, followed by a new line
+ *
+ * return: Nothing
+ */
+
+void puts_half(char *str)
+{
+int len = 0;
+int half = 0;
+len = _strlen(str); /* This includes the null terminator */
+half = (len + 1) / 2;
+while (half <= (len - 1))
+{
+_putchar(*(str + half));
+half++;
+}
+_putchar('\n');
+}
+/**
+ * _strlen - Entry point
+ * @s: String to be counted
+ *
+ * This program counts the length of
+ * a string by counting each character
+ * until the null character is encountered.
+ *
+ * Return: Number of characters in the string.
+ */
+int _strlen(char *s)
+{
+int count = 0;
+while (*(s + count) != '\0')
+{
+count++;
+}
+return (count);
+}
