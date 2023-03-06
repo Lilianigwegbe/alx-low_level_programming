eab012b8069504a65c3dc99a5f5332d03e61e631
lilianigwegbe <lilianc.igwegbe@gmail.com>
2023-03-06 14:40:55 -0800

    101-crackme_password

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * set_string - sets value of a pointer to a char
- * @s: double pointer to a string
- * @to: pointer to the string to set s to
- * Return: Description of the returned value
+ * set_string - sets the value of a pointer to a char.
+ * @s: double pointer to be copied
+ * @to: where s gets set to
  */
 void set_string(char **s, char *to)
 {
