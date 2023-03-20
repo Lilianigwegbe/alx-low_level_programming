aa026f4bfd97b8340dfaa4e30434f863fddd9794
lilianigwegbe <lilianc.igwegbe@gmail.com>
2023-03-20 15:28:47 -0700

    0-main.c

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif 
