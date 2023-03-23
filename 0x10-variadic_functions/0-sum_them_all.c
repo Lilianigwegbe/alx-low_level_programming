8b5ba7d3b112aab5cb07b981bd746f478a0ee602
lilianigwegbe <lilianc.igwegbe@gmail.com>
2023-03-23 12:40:52 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,21 +3,20 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all -  sums all arguments variadic
- * @n: Number of Arguments
- * @...: Arguments Variadic
- *
- * Return: Always 0
+ * sum_them_all - check the code for alx young students.
+ * @n: number of arguments
+ * Return: Always 0.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-unsigned int sum = 0, i;
-va_list list;
-if (n == 0)
-return (0);
-va_start(list, n);
+va_list valist;
+int sum = 0;
+unsigned int i;
+va_start(valist, n);
 for (i = 0; i < n; i++)
-sum += va_arg(list, int);
-va_end(list);
+{
+sum += va_arg(valist, int);
+}
+va_end(valist);
 return (sum);
 }
