92fff573ad05db3f0d479f13a00201b7b94093fa
Lilian Igwegbe <lilianc.igwegbe@gmail.com>
2023-04-04 18:23:49 -0700

    Set Head to Null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list and sets head to NULL
+ * @head:a pointer that points to the head of the list
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *current;
+while (*head != NULL)
+{
+current = *head;
+*head = (*head)->next;
+free(current);
+}
+*head = NULL;
+}
