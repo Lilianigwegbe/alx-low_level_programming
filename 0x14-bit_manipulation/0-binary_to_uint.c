421c6febb44f1f89e6a5d9361ff964e8491198e1
Lilian Igwegbe <lilianc.igwegbe@gmail.com>
2023-04-10 09:30:49 -0700

    Fixed Errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,29 +11,17 @@
  */
 
 unsigned int binary_to_uint(const char *b)
-
 {
 unsigned int value = 0;
 int i = 0;
-unsigned int one = 0;
 if (b == NULL)
 return (0);
 while (b[i] != '\0')
 {
 if (b[i] != '0' && b[i] != '1')
 return (0);
+value = value * 2 + (b[i] - '0');
 i++;
-}
-for (i = 0; b[i] != '\0'; i++)
-{
-if (b[i] == '0')
-one = 0;
-else if (b[i] == '1')
-one = 1;
-else
-return (0);
-one = one << i;
-value = value | one;
 }
 return (value);
 }
