2e95a6c90839dc93de08bc558fb43f9c4609bc90
Lilian Igwegbe <lilianc.igwegbe@gmail.com>
2023-04-10 08:39:27 -0700

    Set Bit and Betty Compliant

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,20 +2,18 @@
 #include "main.h"
 
 /**
- * set_bit - A function that sets the value of a bit to 1 at a given index.
- * @n: a number
+ * set_bit - a function that sets the value of a bit to 1 at a given index.
+ * @n: a pointer to an unsigned long int representing the number
  * @index: is the index, starting from 0 of the bit you want to set
- * Returns: if it works, 1 or if an error occurs, -1
+ * Return: 1 if it is successful or -1, if an error occurs
  *
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int num;
 if (index > 63)
-return -1;
+return (-1);
 num = 1 << index;
 *n = *n | num;
-return 1;
+return (1);
 }
-
-
