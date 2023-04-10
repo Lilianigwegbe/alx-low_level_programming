506a11f1fca3e366beba9addb02f046b98641e88
Lilian Igwegbe <lilianc.igwegbe@gmail.com>
2023-04-10 08:48:58 -0700

    Get Bit and Betty Compliant

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,26 +3,19 @@
 
 /**
 * get_bit - a function that returns a bit value at a given index
-* @index - the index from point 0 of the bit value
-* @n - the origin of the bit value
-* Returns: the bit value at index if it works, or -1 if an error occurrs
+* @index: the index from point 0 of the bit value
+* @n: the origin of the bit value
+* Return: the bit value at index if it works, or -1 if an error occurs
 */
 
-int get_bit(unsigned long int n, unsigned int index)
+signed int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned long int bitsize;
 if (index > 63)
 return (-1);
-bitsize = 1 << index;
+bitsize = 1UL << index;
 if (n & bitsize)
 return (1);
 else
 return (0);
 }
-
-
-
-
-
-
-
