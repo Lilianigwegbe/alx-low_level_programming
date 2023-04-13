7c9e18dcd7d6d5715dfdc07e434e5ec959df95f4
Lilian Igwegbe <lilianc.igwegbe@gmail.com>
2023-04-13 13:38:01 -0700

    Fixed More Code Errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,15 +8,13 @@
  * create_file - a function that creates a file
  *
  * @filename: name of the file to be created
- * @text: a NULL terminated string to write to the file
+ * @text_content: a NULL terminated string to write to the file
  *
  * Return: 1 if successful and -1, if it fails
  */
-
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-{
 if (filename == NULL)
 {
 return (-1);
@@ -37,5 +35,4 @@ return (-1);
 }
 close(fd);
 return (1);
-}
 }
